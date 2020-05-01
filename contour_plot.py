b7aadcdc4d76e8878c0b6c14e6d1ef36d09979a4
Sudheer <ssreedhar@ucdavis.edu>
2020-05-01 01:28:20 -0700

    Discovered the wonders of hist2D.

--- contour_plot.py
+++ contour_plot.py
@@ -1,33 +1,31 @@
-"""Testing for marginalisation and 'contour plotting'.
+"""Marginalisation and 'contour plotting'.
 """
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-def marginalised_histogram(data_variable, bin1, bin2, bin3=10, bin4=10):
-    """Given a 2-parameter space(soon to be 4) data, this function 
-    will plot the histogram of the first dimension by 'integrating'
-    out the 2nd dimension.'data_variable' should be a (N x 2) numpy
-    array. The number of bins for each variable are bin1, bin2, etc.
-    Currently the marginalisation is over the first variable.
-    """
 
-    #Actual histogram evaluation
-    #H is the count corresponding to the bin in edge. 'edges' are the
-    #limits of the 
-    H, edges= np.histogramdd(data_variable, bins=(bin1,bin2),density=1)
-    y=np.sum(H,axis=1)
-    plt.fill_between(edges[0],np.concatenate(([0],y)), step="pre")
+
+#Marginalised 2-D plot
+def marginalised_2d(data_variable, bin1, bin2):
+    """Takes in a (N x M)-sized array,  ignores the last M-2(M>=2) coordinates
+    to produce a normalised histogram of the first 2 variables. 'bin1'
+    and 'bin2' define the number of bins along each direction. 
+    """
+    plt.hist2d(x=data_variable[:, 0],y=data_variable[:, 1], bins=[bin1, bin2], density=1)
     plt.show()
 
 #Fake Data
 x=[]
-for i in range(1000):
-    for j in range(2):
-        col=[]
-        col.append(np.random.normal(2,0.5))
-        col.append(np.random.normal(10,0.5))
+for i in range(10000):
+    col=[]
+
+    col.append(np.random.normal(np.random.choice([2,20]), 0.5))
+    col.append(np.random.normal(10, 0.5))
+    col.append(np.random.normal(-2, 0.5))
+    col.append(np.random.normal(-5, 0.5))
     x.append(col) 
 x=np.array(x)
 
-marginalised_histogram(x,10,5)
+#Code execution
+marginalised_2d(x, 40, 40)
