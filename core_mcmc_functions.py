420fbd8a6199d66733113fe6fecd15ddeadaa502
Kyle Ray <kylejray@gmail.com>
2020-05-10 17:04:19 -0700

    fixed syntax error

--- core_mcmc_functions.py
+++ core_mcmc_functions.py
@@ -366,23 +366,25 @@ def plot_chain_behaviour(
 
     plt.show()
 
-def estimate_covariance(chain, scaling=1, trim_ratio=.25):
-"""
-params
------
-chain: an array where the variables are different columns and rows are observations
-    we will estimate the covariance b/w these variables for this data set
-scaling: float
-    scale the maximum value in the covariance matrix to be this number, genrally <=1
-trim_ratio: float > 0 and < 1
-    this is the ratio of data that we want to drop before looking at covariance
 
-returns
--------
-cov: N X N np array, N is the number of columns in the input chain
-    this is the covariance matrix
-"""
-    cutoff = int(trim_ratio*len(chain[:,0]))
-    cchn = chn[cutoff:,:] 
-    cov=np.cov(cchn, y=None, rowvar=False)
-    return scaling* cov/np.max(np.abs(cov))
+def estimate_covariance(chain, scaling=1, trim_ratio=0.25):
+    """
+    params
+    -----
+    chain: an array where the variables are different columns and rows are observations
+        we will estimate the covariance b/w these variables for this data set
+    scaling: float
+        scale the maximum value in the covariance matrix to be this number, genrally <=1
+    trim_ratio: float > 0 and < 1
+        this is the ratio of data that we want to drop before looking at covariance
+
+    returns
+    -------
+    cov: N X N np array, N is the number of columns in the input chain
+        this is the covariance matrix
+    """
+    cutoff = int(trim_ratio * len(chain[:, 0]))
+    cchn = chn[cutoff:, :]
+    cov = np.cov(cchn, y=None, rowvar=False)
+    return scaling * cov / np.max(np.abs(cov))
+
